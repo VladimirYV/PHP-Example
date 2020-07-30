4317129fa10f613a842b66447262c101fa661d41
Vladimir Yezhuninov <vladimir.vy77@gmail.com>
2020-07-30 08:45:10 +0300

    new line

--- loops.php
+++ loops.php
@@ -1,6 +1,22 @@
 <?php
+
+
+
 	$arr = [1, 2, 3, 4, 5]; //этот массив дан
     foreach ($arr as $elem) {
         echo $elem.'<br>';
     }
-    
+  
+    //ассоциативный массив
+    unset($arr);
+
+    $arr = ['a'=>1, 'b'=>2, 'c'=>3, 'd'=>4, 'e'=>5];
+    foreach ($arr as $key=>$elem) {
+        echo "<br>".$key."-".$elem;
+    }
+
+$a = 56;
+$b = 12;
+$c = (int) ($a / $b);
+echo $c;
+?>
