8914b9ca742462e6f86f7ef6a114d74c638287da
Vladimir Yezhuninov <vladimir.vy77@gmail.com>
2020-08-19 15:14:25 +0300

    authenticate examples

---
+++ auth.php
@@ -0,0 +1,17 @@
+<?php
+$username = 'admin';
+$password = 'letmein';
+if (isset($_SERVER['PHP_AUTH_USER']) &&
+isset($_SERVER['PHP_AUTH_PW']))
+{
+if ($_SERVER['PHP_AUTH_USER'] == $username && $_SERVER['PHP_AUTH_PW'] == $password)
+echo "Регистрация прошла успешно";
+else die("Неверная комбинация имя пользователя — пароль");
+}
+else
+{
+header('WWW-Authenticate: Basic realm="Restricted Section"');
+header('HTTP/1.0 401 Unauthorized');
+die ("Пожалуйста, введите имя пользователя и пароль");
+}
+?>
