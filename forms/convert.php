d20da79e625db5d47caff4dc1b7dd306285ab829
Vladimir Yezhuninov <vladimir.vy77@gmail.com>
2020-08-19 14:24:41 +0300

    form examples

--- forms/convert.php
+++ forms/convert.php
@@ -44,3 +44,18 @@ return $var;
 <form action='myscript.php' method='post' id='form1'>
 </form>
 <input type='text' name='username' form='form1'>
+<br>
+Выберите нужный сайт:
+<input type='url' name='site' list='links'>
+<datalist id='links'>
+<option label='Google' value='http://google.com'>
+<option label='Yahoo!' value='http://yahoo.com'>
+<option label='Bing' value='http://bing.com'>
+<option label='Ask' value='http://ask.com'>
+</datalist><br>
+Выберите цвет <input type='color' name='color'><br>
+<input type='time' name='meeting' value='12:00'
+min='09:00' max='16:00' step='3600'><br>
+<input type='number' name='age'>
+<input type='range' name='num' min='0' max='100' value='50' step='1'>
+<input type='time' name='time' value='12:34'>
